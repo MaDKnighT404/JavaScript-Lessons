451f53ef3a959328c1fee507e6f96d2492664f04
MaDKnight404 <battleaxe@bk.ru>
2022-03-20 18:50:40 +0300

    2) часть задания

--- 1/Home1.js
+++ 1/Home1.js
@@ -41,3 +41,12 @@
 
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
